007b863224c34ddb1362369324d6fb4fcad9160f
Wiro17 <wrsoccer01@gmail.com>
2016-06-08 18:16:34 -0400

    Add files via upload

---
+++ Enemies.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class Enemies here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Enemies extends Character
+{
+    public Enemies()
+    {
+    }
+}
