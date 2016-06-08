7a4bf1ffb284f8e4384a7f25dcd13726e18ac20b
Wiro17 <wrsoccer01@gmail.com>
2016-06-08 18:18:07 -0400

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,6 +1,6 @@
 package Main;
 
-import java.awt.event.ActionEvent;
+mport java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.Timer;
@@ -10,42 +10,55 @@ import Graphics.Screen;
 import Map.Map;
 
 public class Game implements ActionListener{
-	private static boolean inGame;
-	private static Screen screen;
-	private static Game game;
-	private static Timer timer;
-	private static Map map;
-	private static Player player;
-	
-	public static void main(String[] args){
-		screen = new Screen();
-		game = new Game();
-		timer = new Timer(100, game);
-		map = new Map();
-		timer.start();
-	}
-	
-	public static void enterGame() {
-		inGame = true;
-	}
-	
-	public static void exitGame() {
-		inGame = false;
-	}
-	
-	public static boolean getGameState() {
-		return inGame;
-	}
-	
-	public void actionPerformed(ActionEvent e) {
-		screen.repaint();
-	}
-	
-	public static Map getMap() {
-		return map;
-	}
-	
-	public static Player getPlayer() {
-		return player;
-	}
+    private static boolean inGame;
+    private static Screen screen;
+    private static Game game;
+    private static Timer timer;
+    private static Map map;
+    private static Player player;
+    private int currentRoom;
+ 
+    
+    
+    public static void main(String[] args){
+        screen = new Screen();
+        game = new Game();
+        timer = new Timer(100, game);
+        map = new Map();
+        
+        while(inGame == true)
+        {
+            timer.start();
+            player = new Player();
+            currentRoom = 1;
+            
+            
+        }
+    }
+
+    public static void enterGame() {
+        inGame = true;
+    }
+
+    public static void exitGame() {
+        inGame = false;
+    }
+
+    public static boolean getGameState() {
+        return inGame;
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        screen.repaint();
+    }
+
+    public static Map getMap() {
+        return map;
+    }
+
+    public static Player getPlayer() {
+        return player;
+    }
+    
+    
 }
