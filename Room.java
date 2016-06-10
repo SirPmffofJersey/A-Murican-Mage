51ed8f30a2936c98e88816c0ba1f87a73a46ff91
SirPmffofJersey <ndn1x1@gmail.com>
2016-06-10 13:10:39 -0400

    Update Room.java

--- Room.java
+++ Room.java
@@ -9,7 +9,7 @@ import java.awt.*;
 public class Room
 {
     //fields    
-    private Obstacle[] objects;
+    private Obstacle[] objects; //RULES FOR THIS ARRAY: the first element is the door that would come from a lower number room, and if there is a second door in the room it is the second element in the array
     private Rectangle dimensions;
     private String imageName;    
     private int roomIndex;
@@ -39,18 +39,79 @@ public class Room
       objects[1].setLoc(2*40,11*40); //top door
       break;
       
+      case 2: //Room 2
+      dimensions = new Rectangle(SIZE4);
+      objects = new Obstacle[]{new Doorway(1), new Doorway(3)};
+      objects[0].setLoc(6*40,2*40); //bottom door
+      objects[1].setLoc(3*40,6*40); //right door
+      break;
       
-     }
+      case 3: //Room 3
+      dimensions = new Rectangle(SIZE1);
+      objects = new Obstacle[]{new Doorway(2), new Doorway(4)};
+      objects[0].setLoc(2*40,0); //left door
+      objects[1].setLoc(2*40,9*40); //right door
+      break;
+      
+      case 4: //Room 
+      dimensions = new Rectangle(SIZE4);
+      objects = new Obstacle[]{new Doorway(3), new Doorway(5)};
+      objects[0].setLoc(3*40,0); //left door
+      objects[1].setLoc(0,3*40); //top door
+      break;
+      
+      case 5: //Room 5
+      dimensions = new Rectangle(SIZE3);
+      objects = new Obstacle[]{new Doorway(4), new Doorway(6)};
+      objects[0].setLoc(9*40,7*40); //bottom door
+      objects[1].setLoc(5*40,0); //left door
+      break;
+      
+      case 6: //Room 6
+      dimensions = new Rectangle(SIZE2);
+      objects = new Obstacle[]{new Doorway(5), new Doorway(7)};
+      objects[0].setLoc(2*40,14*40); //right door
+      objects[1].setLoc(0,2*40); //top door
+      break;
+      
+      case 7: //Room 7
+      dimensions = new Rectangle(SIZE1);
+      objects = new Obstacle[]{new Doorway(6), new Doorway(8)};
+      objects[0].setLoc(2*40,0); //bottom door
+      objects[1].setLoc(2*40,9*40); //right door
+      break;
+      
+      case 8: //Room 8
+      dimensions = new Rectangle(SIZE4);
+      objects = new Obstacle[]{new Doorway(7), new Doorway(9)};
+      objects[0].setLoc(3*40,0); //left door
+      objects[1].setLoc(0,3*40); //top door
+      break;
+      
+      case 9: //Room 9
+      dimensions = new Rectangle(SIZE3);
+      objects = new Obstacle[]{new Doorway(8)}
+      objects[0].setLoc(9*40,7*40);
+      break;
+      
+      default: //if all else fails, make it the start room.
+      dimensions = new Rectangle(SIZE4);
+      objects = new Obstacle[]{new Doorway(1)};
+      objects[0].setLoc(3*40,6*40); //right door
+      break;
+     }     
     }
     
     //methods
     public Obstacle getObstacle(int index)
     {return objects[index];}
             
-    public Point getEnteredDoor(Point p) //purpose: so as to find out where to place the player
-    {return }
+    public Point getEnteredDoorLocation(Rectangle ricky) //purpose: so as to find out where to place the player
+    {return ricky.getLocation();}
     
     public Rectangle getDim()
     {return dimensions;}    
     
+    public String getImageName()
+    {return imageName();}
 }
