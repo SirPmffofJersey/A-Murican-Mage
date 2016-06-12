cb251c7a39375bcf59880b59830baf2452de1313
SirPmffofJersey <ndn1x1@gmail.com>
2016-06-12 13:17:05 -0700

    Update Room.java

--- Room.java
+++ Room.java
@@ -29,75 +29,75 @@ public class Room
       case 0: //Start room
       dimensions = new Rectangle(SIZE4);
       objects = new Obstacle[]{new Doorway(1)};
-      objects[0].setLoc(3*40,6*40); //right door
+      objects[0].setLoc(3,6); //right door
       break;
       
       case 1: //Room 1
       dimensions = new Rectangle(SIZE2);
       objects = new Obstacle[]{new Doorway(0), new Doorway(2)};
-      objects[0].setLoc(2*40,0); //left door
-      objects[1].setLoc(0,11*40); //top door
+      objects[0].setLoc(2,0); //left door
+      objects[1].setLoc(0,11); //top door
       break;
       
       case 2: //Room 2
       dimensions = new Rectangle(SIZE4);
       objects = new Obstacle[]{new Doorway(1), new Doorway(3)};
-      objects[0].setLoc(6*40,2*40); //bottom door
-      objects[1].setLoc(3*40,6*40); //right door
+      objects[0].setLoc(6,2); //bottom door
+      objects[1].setLoc(3,6); //right door
       break;
       
       case 3: //Room 3
       dimensions = new Rectangle(SIZE1);
       objects = new Obstacle[]{new Doorway(2), new Doorway(4)};
-      objects[0].setLoc(2*40,0); //left door
-      objects[1].setLoc(2*40,9*40); //right door
+      objects[0].setLoc(2,0); //left door
+      objects[1].setLoc(2,9); //right door
       break;
       
       case 4: //Room 
       dimensions = new Rectangle(SIZE4);
       objects = new Obstacle[]{new Doorway(3), new Doorway(5)};
-      objects[0].setLoc(3*40,0); //left door
-      objects[1].setLoc(0,3*40); //top door
+      objects[0].setLoc(3,0); //left door
+      objects[1].setLoc(0,3); //top door
       break;
       
       case 5: //Room 5
       dimensions = new Rectangle(SIZE3);
       objects = new Obstacle[]{new Doorway(4), new Doorway(6)};
-      objects[0].setLoc(9*40,7*40); //bottom door
-      objects[1].setLoc(5*40,0); //left door
+      objects[0].setLoc(9,7); //bottom door
+      objects[1].setLoc(5,0); //left door
       break;
       
       case 6: //Room 6
       dimensions = new Rectangle(SIZE2);
       objects = new Obstacle[]{new Doorway(5), new Doorway(7)};
-      objects[0].setLoc(2*40,14*40); //right door
-      objects[1].setLoc(0,2*40); //top door
+      objects[0].setLoc(2,14); //right door
+      objects[1].setLoc(0,2); //top door
       break;
       
       case 7: //Room 7
       dimensions = new Rectangle(SIZE1);
       objects = new Obstacle[]{new Doorway(6), new Doorway(8)};
-      objects[0].setLoc(2*40,0); //bottom door
-      objects[1].setLoc(2*40,9*40); //right door
+      objects[0].setLoc(2,0); //bottom door
+      objects[1].setLoc(2,9); //right door
       break;
       
       case 8: //Room 8
       dimensions = new Rectangle(SIZE4);
       objects = new Obstacle[]{new Doorway(7), new Doorway(9)};
-      objects[0].setLoc(3*40,0); //left door
-      objects[1].setLoc(0,3*40); //top door
+      objects[0].setLoc(3,0); //left door
+      objects[1].setLoc(0,3); //top door
       break;
       
       case 9: //Room 9
       dimensions = new Rectangle(SIZE3);
       objects = new Obstacle[]{new Doorway(8)}
-      objects[0].setLoc(9*40,7*40);
+      objects[0].setLoc(9,7);
       break;
       
       default: //if all else fails, make it the start room.
       dimensions = new Rectangle(SIZE4);
       objects = new Obstacle[]{new Doorway(1)};
-      objects[0].setLoc(3*40,6*40); //right door
+      objects[0].setLoc(3,6); //right door
       break;
      }     
     }
