f937ceb3aeb629c4497aca662ca2980dbcc52467
aquaTurtle1358 <ddfplay@gmail.com>
2016-06-13 11:30:41 -0400

    Update Potion.java

--- Potion.java
+++ Potion.java
@@ -1,5 +1,23 @@
 package Item;
 
 public class Potion {
-
+  private boolean equipped;
+  private int type;
+  private final FILL_VALUE = 50;
+  
+  public Potion(int t) {
+    type = t;
+  }
+  
+  public boolean getEquipped(){return equipped;}
+  
+  public void setEquipped(boolean e){equipped = e};
+  
+  public void use(Charactor c) {
+    if(type == 0) {
+      c.setHealth(Math.min(c.getHealth() + FILL_VALUE, c.getMaxHealth));
+    } else {
+      c.setMana(Math.min(c.getMana() + FILL_VALUE, c.getMaxMana()));
+    }
+  }
 }
