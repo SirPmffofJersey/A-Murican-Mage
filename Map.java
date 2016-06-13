70b2d885ce28417a15160c62d50ed1c329cc7249
SirPmffofJersey <ndn1x1@gmail.com>
2016-06-13 15:17:21 -0400

    Update Map.java

--- Map.java
+++ Map.java
@@ -12,14 +12,22 @@ public class Map
    private int currentRoomIndex;
    
    //constructor
-   public Map()
+    public Map()
    {
-    map = new Room[]{}
+    map = new Room[]{new Room(0), new Room(1), new Room(2), new Room(3),
+                     new Room(4), new Room(5), new Room(6), new Room(7),
+                     new Room(8), new Room(9)};
+    currentRoom = 0;                 
    }
    
    //methods
    public Room getRoom(int roomIndex)
    {return map[roomIndex];}
+   
+   public Room getCurrentRoom()
+   {
+    return map[getCurrentRoomIndex()];
+   }
    
     public int getCurrentRoomIndex()
    {return currentRoomIndex;}
