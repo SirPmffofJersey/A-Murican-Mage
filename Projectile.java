63ba85df2cec329b5f7f33cd1aecb4ed728f6106
aquaTurtle1358 <ddfplay@gmail.com>
2016-06-14 16:15:33 -0400

    Update Projectile.java

--- Projectile.java
+++ Projectile.java
@@ -12,16 +12,34 @@ public class Projectile {
 	private int xRange;
 	private int yRange;
 	private String image;
-	private int speed;
+	private double distanceToTravel;
 	private double angle;
+	private boolean toBeRemoved;
 	
 	public Projectile(Spell t, Point mouse) {
 		type = t;
 		angle = Math.atan(Game.getPlayer().getDimensions().getCenterY() - mouse.getY()/Game.getPlayer().getDimensions().getCenterX() - mouse.getX());
 		yRange = (int) (t.getRange() * Math.sin(Math.toRadians(angle)));
 		xRange = (int) (t.getRange() * Math.cos(Math.toRadians(angle)));
+		int speed = 0;
+		if(t.getName().equals("FireLobber"))
+			speed = 5;
+		else
+			speed = 10;
+		distanceToTravel = speed/t.getRange();
 		if(type == t)
-			;
-			//dim = new Rectangle(Game.getPlayer().getDimensions().getX(), );
+			dim = new Rectangle((int)Game.getPlayer().getDimensions().getX(), (int)Game.getPlayer().getDimensions().getY(), 50, 50);
+		while(dim.intersects(Game.getPlayer().getDimensions()))
+			move();
 	}
+	
+	public void move() {
+		dim.setLocation((int)(dim.getX() + xRange * distanceToTravel), (int)(dim.getX() + yRange * distanceToTravel));
+		if(dim.getX() > xRange || dim.getY() > yRange)
+			toBeRemoved = true;
+	}
+	
+	public boolean toBeRemoved() {return toBeRemoved;}
+	
+	public Rectangle getDim() {return dim;}
 }
