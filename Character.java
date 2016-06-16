a8f43e0155486350db031a83bc783082f51c8c5a
aquaTurtle1358 <ddfplay@gmail.com>
2016-06-15 22:43:35 -0400

    Update Character.java

--- Character.java
+++ Character.java
@@ -1,11 +1,15 @@
 import java.awt.Image;
 import java.awt.Point;
 import java.awt.Rectangle;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.swing.ImageIcon;
 
 import Item.Sword;
 import Main.Game;
+import Map.Doorway;
+import Map.Obstacle;
 import Map.Room;
 
 /**
@@ -28,13 +32,13 @@ public class Character {
 	private int direction;
 	private int state;
 	private int seconds;
-	 private boolean immune = false; 
+	private boolean immune = false;
 	// private Buffs[] buffs;
 	private Rectangle dimensions;
-	private Point location;
+	private Rectangle nextDim;				//Used for collisions
 	private Image image;
 	private Sword sword;
-	public Character(String name, int hp, int mp, int maxHp, int maxMp, int atk, int def, int spd, int lvl, Point loc,
+	public Character(String name, int hp, int mp, int maxHp, int maxMp, int atk, int def, int spd, int lvl,
 			Rectangle dim, Sword s, int direct) {
 		this.name = name;
 		health = hp;
@@ -45,13 +49,12 @@ public class Character {
 		defense = def;
 		speed = spd;
 		level = lvl;
-		location = loc;
 		dimensions = dim;
+		nextDim = dim;
 		sword = s;
 		direction = direct;
 		state = 0;
 		image = new ImageIcon(getClass().getResource("/" + name + direction + ".gif")).getImage();
-		seconds = 0;
 	}
 
 	// Accessor Methods
@@ -107,10 +110,6 @@ public class Character {
 		return sword;
 	}
 
-	public Point getLocation() {
-		return location;
-	}
-
 	// Modifying Methods
 	public void setHealth(int hp) {
 		health = hp;
@@ -149,8 +148,14 @@ public class Character {
 		resetImage();
 	}
 
-	public void setDimensions(int x, int y) {
-		dimensions = new Rectangle(x, y);
+	public void setDimensions(int w, int h) {
+		int x = (int)dimensions.getX();
+		int y = (int)dimensions.getY();
+		dimensions = new Rectangle(x, y, w, h);
+	}
+	
+	public void setDimensions(int x, int y, int w, int h) {
+		dimensions = new Rectangle(x, y, w, h);
 	}
 
 	/*
@@ -166,7 +171,12 @@ public class Character {
 	}
 
 	public void setLocation(Point loc) {
-		location = loc;
+		nextDim = new Rectangle((int)loc.getX(), (int)loc.getY(), (int)dimensions.getWidth(), (int)dimensions.getHeight());
+		System.out.println("" + dimensions.getX() + " " + dimensions.getY() + " " + dimensions.getWidth() + " " + dimensions.getHeight() + " " + nextDim.getX() + " " + nextDim.getY() + " " + nextDim.getWidth() + " " + nextDim.getHeight());
+		for(int i = 0; i < Game.getMap().getCurrentRoom().getNumberOfObstacles(); i++)
+			collision(Game.getMap().getCurrentRoom().getObstacle(i));
+		dimensions = nextDim;
+		collision(Game.getPlayer().getSword(), loc);
 	}
 
 	public int movementState() {
@@ -174,9 +184,9 @@ public class Character {
 	}
 
 	public void attack(Character other) {
-		if (other.location.getX() - location.getX() <= sword.getRange()
-				|| other.location.getY() - location.getY() <= sword.getRange()
-				|| (other.location.getY() - location.getY()) / (other.location.getX() - location.getX()) <= sword
+		if (other.dimensions.getX() - dimensions.getX() <= sword.getRange()
+				|| other.dimensions.getY() - dimensions.getY() <= sword.getRange()
+				|| (other.dimensions.getY() - dimensions.getY()) / (other.dimensions.getX() - dimensions.getX()) <= sword
 						.getRange()) {
 			other.setHealth(other.getHealth() - sword.getDamage());
 		}
@@ -190,7 +200,7 @@ public class Character {
 	}
 
 	public void move() {
-		location.move((int) location.getX() + speed, (int) location.getY() + speed);
+		//dimensions.move((int) location.getX() + speed, (int) location.getY() + speed);
 	}
 
 	public void attacked(int a) {
@@ -212,11 +222,12 @@ public class Character {
                     		timer.purge(); 
                 	}
             	}
-        
+        	};
         	timer.schedule(task,250); 
         	//seconds = 0; 
 	}
-
+	
+	/*
 	public boolean collision(Character c) {
 		int coordinateX = Math.abs((int) c.getDimensions().getX() - (int) location.getX());
 		int coordinateY = Math.abs((int) c.getDimensions().getY() - (int) location.getY());
@@ -225,24 +236,73 @@ public class Character {
 			return true;
 		return false;
 	}
+	*/
+	
+	public boolean collision(Character c) {
+		return false;
+	}
+	
+	public boolean collision(Sword s, Point p) {
+		if(s.getDim() == null)
+			return false;
+		if(s.getDim().intersects(dimensions)) {
+			attacked(Game.getPlayer().getAttack() + s.getDamage());
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean collision(Obstacle o) {
+		Doorway d = null;
+		if(o instanceof Doorway) {
+			d = (Doorway)o;
+			if(d.getEnabled())
+				d.travel();
+			else
+				return false;
+		} else {
+			if(o.getDimensions().intersects(nextDim)) {
+				System.out.println("Intersection");
+				if(o.getDimensions().getX() >= dimensions.getX() + dimensions.getWidth() && o.getDimensions().getX() < nextDim.getX() + nextDim.getWidth()) {
+					System.out.println("Collision");
+					nextDim = new Rectangle((int)(o.getDimensions().getX() - nextDim.getWidth()), (int)nextDim.getY(), (int)nextDim.getWidth(), (int)nextDim.getHeight());
+					return true;
+				} else if(o.getDimensions().getX() + o.getDimensions().getWidth() <= dimensions.getX() && o.getDimensions().getX() + o.getDimensions().getWidth() > nextDim.getX()) {
+					System.out.println("Collision");
+					nextDim = new Rectangle((int)(o.getDimensions().getX() + o.getDimensions().getWidth()), (int)nextDim.getY(), (int)nextDim.getWidth(), (int)nextDim.getHeight());
+					return true;
+				} else if(o.getDimensions().getY() >= dimensions.getY() + dimensions.getHeight() && o.getDimensions().getY() < nextDim.getY() + nextDim.getHeight()) {
+					System.out.println("Collision");
+					nextDim = new Rectangle((int)nextDim.getX(), (int)(o.getDimensions().getY() - nextDim.getHeight()), (int)nextDim.getWidth(), (int)nextDim.getHeight());
+					return true;
+				} else if(o.getDimensions().getY() + o.getDimensions().getHeight() <= dimensions.getY() && o.getDimensions().getY() + o.getDimensions().getHeight() > nextDim.getY()) {
+					System.out.println("Collision");
+					nextDim = new Rectangle((int)nextDim.getX(), (int)(o.getDimensions().getY() + o.getDimensions().getHeight()), (int)nextDim.getWidth(), (int)nextDim.getHeight());
+					return true;
+				}
+			} else
+				return false;
+		}
+		return false;
+	}
 
 	public void hitWall() {
 		int maxX = (int)(Game.getMap().getCurrentRoom().getDim().getX() + Game.getMap().getCurrentRoom().getDim().getWidth() - dimensions.getWidth());
 		int maxY = (int)(Game.getMap().getCurrentRoom().getDim().getY() + Game.getMap().getCurrentRoom().getDim().getHeight() - dimensions.getHeight());
-		if (location.getX() >= maxX)
-			setLocation(new Point(maxX, (int) location.getY()));
-		if (location.getX() <= 0)
-			setLocation(new Point(0, (int) location.getY()));
-		if (location.getY() >= maxY)
-			setLocation(new Point((int) location.getX(), maxY));
-		if (location.getY() <= 0)
-			setLocation(new Point((int) location.getX(), 0));
+		if (dimensions.getX() >= maxX)
+			setLocation(new Point(maxX, (int) dimensions.getY()));
+		if (dimensions.getX() <= 0)
+			setLocation(new Point(0, (int) dimensions.getY()));
+		if (dimensions.getY() >= maxY)
+			setLocation(new Point((int) dimensions.getX(), maxY));
+		if (dimensions.getY() <= 0)
+			setLocation(new Point((int) dimensions.getX(), 0));
 
 	}
 
 	public boolean canAttack(Character c) {
-		int coordinateX = Math.abs((int) c.getDimensions().getX() - (int) location.getX());
-		int coordinateY = Math.abs((int) c.getDimensions().getY() - (int) location.getY());
+		int coordinateX = Math.abs((int) c.getDimensions().getX() - (int) dimensions.getX());
+		int coordinateY = Math.abs((int) c.getDimensions().getY() - (int) dimensions.getY());
 		int distance = coordinateX + coordinateY;
 		if (distance <= 100)
 			return true;
@@ -250,8 +310,8 @@ public class Character {
 	}
 
 	public boolean canUseMagic(Character c, int manaCost) {
-		int coordinateX = Math.abs((int) c.getDimensions().getX() - (int) location.getX());
-		int coordinateY = Math.abs((int) c.getDimensions().getY() - (int) location.getY());
+		int coordinateX = Math.abs((int) c.getDimensions().getX() - (int) dimensions.getX());
+		int coordinateY = Math.abs((int) c.getDimensions().getY() - (int) dimensions.getY());
 		int distance = coordinateX + coordinateY;
 		if (distance <= 100 && mana > manaCost)
 			return true;
@@ -267,26 +327,28 @@ public class Character {
 
 	public void moveTowardPlayer(Character c) {
 		int newD = 0;
-		if (c.getLocation().getX() > (int) location.getX()) {
-			setLocation(new Point((int) location.getX() + 10, (int) location.getY()));
+		if (c.getDimensions().getX() > (int) dimensions.getX()) {
+			setLocation(new Point((int) dimensions.getX() + 10, (int) dimensions.getY()));
 			newD = 2;
 			hitWall();
-		} else if (c.getLocation().getX() < (int) location.getX()) {
-			setLocation(new Point((int) location.getX() - 10, (int) location.getY()));
+		} else if (c.getDimensions().getX() < (int) dimensions.getX()) {
+			setLocation(new Point((int) dimensions.getX() - 10, (int) dimensions.getY()));
 			newD = 4;
 			hitWall();
 		}
-		if (c.getLocation().getY() > (int) location.getY()) {
-			setLocation(new Point((int) location.getX(), (int) location.getY() + 10));
+		if (c.getDimensions().getY() > (int) dimensions.getY()) {
+			setLocation(new Point((int) dimensions.getX(), (int) dimensions.getY() + 10));
 			newD = 3;
 			hitWall();
-		} else if (c.getLocation().getY() < (int) location.getY()) {
-			setLocation(new Point((int) location.getX(), (int) location.getY() - 10));
+		} else if (c.getDimensions().getY() < (int) dimensions.getY()) {
+			setLocation(new Point((int) dimensions.getX(), (int) dimensions.getY() - 10));
 			newD = 1;
 			hitWall();
 		}
 		
-		if(newD != 0 || newD != getDirection())
+		if(newD == 0)
+			setDirection(newD);
+		if(newD != getDirection())
 			setDirection(newD);
 
 		/*
@@ -301,7 +363,7 @@ public class Character {
 	
 	public boolean insideMap(int roomNum)
     	{
-        	return location.getX() > new Room(roomNum).getDim().getX() && location.getY() > new Room(roomNum).getDim().getY() && location.getX() < new Room(roomNum).getDim().getX() + new Room(roomNum).getDim().getWidth() && location.getY() < new Room(roomNum).getDim().getY() + new Room(roomNum).getDim().getHeight();
+        	return dimensions.getX() > new Room(roomNum).getDim().getX() && dimensions.getY() > new Room(roomNum).getDim().getY() && dimensions.getX() < new Room(roomNum).getDim().getX() + new Room(roomNum).getDim().getWidth() && dimensions.getY() < new Room(roomNum).getDim().getY() + new Room(roomNum).getDim().getHeight();
     	}
 
 }
