9e8347c2e41c08e41d07aa1f1b9d06a853d12e91
aquaTurtle1358 <ddfplay@gmail.com>
2016-06-16 16:12:08 -0400

    Update Sword.java

--- Sword.java
+++ Sword.java
@@ -3,7 +3,7 @@ package Item;
 import java.awt.Image;
 import java.awt.Rectangle;
 
-public class Sword implements Item{
+public class Sword {
 	private Rectangle dim;
 	private String image;
 	private boolean equipped;
