06edba477f08f024bec219299332ec2904ff90e8
aquaTurtle1358 <ddfplay@gmail.com>
2016-06-19 01:55:42 -0400

    Update Crunch.java

--- Crunch.java
+++ Crunch.java
@@ -5,21 +5,51 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
-import java.awt.Point;
 import java.awt.Rectangle;
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import Item.Sword;
+import Main.Game;
 public class Crunch extends Bosses 
 {
-    int numSpawn = 5; 
-    public Crunch(String name, int hp, int mp, int maxHp, int maxMp,int atk, int def, int spd, int lvl, Point loc, Rectangle dim, Item e,int direct)
+	
+    public Crunch(String name, int hp, int mp, int maxHp, int maxMp,int atk, int def, int spd, int lvl, Rectangle dim, Sword e,int direct)
     {
-        super(name,hp,mp,maxHp,maxMp,atk,def,spd,lvl,loc,dim,e,direct);
+    	
+        super(name,hp,mp,maxHp,maxMp,atk,def,spd,lvl,dim,e,direct);
     }
     
-    public void spawnEnemies()
-    {
-        for(int i = 0; i < numSpawn; i++) 
-        {
-            //spawnEnemies method 
-        }
+    public void spawnEnemies(ArrayList<Enemies> e, Character c){
+    	final Timer timer = new Timer();
+        timer.schedule(new TimerTask(){
+           	public void run(){
+           		
+                e.add(new Enemies("Police officer", 50, 50, 50, 50, 5, 20, 2, 1, new Rectangle((int)(Math.random()* 1200 + 100), (int)(Math.random()* 700 + 100), 100, 100), new Sword(""), 1)); 	
+           	
+		        if(c.getHealth() <= 0){
+		        	timer.cancel();
+		            timer.purge(); 
+		        }
+           	}
+        }, 2000, 2000);
+        
+        
+    }  
+    
+    public boolean isDead() {
+		if (getHealth() <= 0) {
+			Game.getPlayer().getLoot((Enemies)this);
+			Game.getPlayer().addExperiance();
+			Game.getScreen().getGameGraphics().addString("You Beat Cap'n Crunchstie");
+			Game.getScreen().getGameGraphics().addString("You unlocked the saber");
+			return true;
+		}
+		return false;
+	}
+    
+    public String dropLoot() {
+    	return "Saber";
     }
 }
