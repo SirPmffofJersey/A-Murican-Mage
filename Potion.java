1a9e2c79b2fe88df60def715e6363fb195dd8885
aquaTurtle1358 <ddfplay@gmail.com>
2016-06-19 02:02:04 -0400

    Update Potion.java

--- Potion.java
+++ Potion.java
@@ -1,23 +1,48 @@
 package Item;
 
 public class Potion {
-  private boolean equipped;
-  private int type;
-  private final FILL_VALUE = 50;
-  
-  public Potion(int t) {
-    type = t;
-  }
-  
-  public boolean getEquipped(){return equipped;}
-  
-  public void setEquipped(boolean e){equipped = e};
-  
-  public void use(Charactor c) {
-    if(type == 0) {
-      c.setHealth(Math.min(c.getHealth() + FILL_VALUE, c.getMaxHealth));
-    } else {
-      c.setMana(Math.min(c.getMana() + FILL_VALUE, c.getMaxMana()));
-    }
-  }
+	private final int FILL_VALUE = 50;
+	private int quantity;
+	private int type;
+	private boolean equipped;
+	
+	public Potion(int t) {
+		type = t;
+		quantity = 1;
+		equipped = false;
+	}
+	
+	public Potion(int t, int q) {
+		type = t;
+		quantity = q;
+	}
+	
+	public void addPotion() {
+		quantity++;
+	}
+	
+	public void addPotion(int num) {
+		quantity += num;
+	}
+	
+	public int getType() {
+		return type;
+	}
+	
+	public int getQuantity() {return quantity;}
+	
+	public boolean getEquipped(){return equipped;}
+	  
+	public void setEquipped(boolean e){equipped = e;}
+	
+	public void use(Charactor.Character c) {
+		if(quantity <= 0) 
+			return;
+		else if( type == 0)
+			c.setHealth(Math.min(c.getHealth() + FILL_VALUE, c.getMaxHealth()));
+		else
+			c.setMana(Math.min(c.getMana() + FILL_VALUE, c.getMaxMana()));
+		quantity--;
+	}
 }
+
