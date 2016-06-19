8fa00d512c86151a0f28902d3f6b7df8dbd72931
aquaTurtle1358 <ddfplay@gmail.com>
2016-06-19 01:56:16 -0400

    Update Cultists.java

--- Cultists.java
+++ Cultists.java
@@ -1,37 +1,27 @@
-import java.util.Timer;
-import java.awt.Canvas;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.image.BufferStrategy;
-import java.awt.event.*;
-import javax.swing.*;
-import java.util.TimerTask;
-import java.awt.Color;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import java.awt.Point;
 import java.awt.Rectangle;
+
+import Item.Sword;
+import Spell.Buff;
 public class Cultists extends Enemies
 {
-    private static int spellAttack; 
-    public Cultists(String name, int hp, int mp, int maxHp, int maxMp,int atk, int def, int spd, int lvl, Point loc, Rectangle dim, Item e,int direct)
+    private Buff b;
+    
+    public Cultists(String name, int hp, int mp, int maxHp, int maxMp,int atk, int def, int spd, int lvl, Rectangle dim, Sword e,int direct)
     {
-        super(name,hp,mp,maxHp,maxMp,atk,def,spd,lvl,loc,dim,e,direct);
-    }
-
-
-    public void castSpell(Character c)
-    {
-        c.attacked(super.getAttack());
+        super(name,hp,mp,maxHp,maxMp,atk,def,spd,lvl,dim,e,direct);
+        b = new Buff("stun");
     }
     
     public boolean canAttack(Character c)
     {
-        int coordinateX = Math.abs((int)(c.getLocation().getX() - super.getLocation().getX()));
-        int coordinateY = Math.abs((int)(c.getLocation().getY() - super.getLocation().getY()));
+        int coordinateX = Math.abs((int)(c.getDimensions().getX() - super.getDimensions().getX()));
+        int coordinateY = Math.abs((int)(c.getDimensions().getY() - super.getDimensions().getY()));
         int distance = coordinateX + coordinateY; 
         if(distance <= 200) 
-            return true;
+        	if(getWait() <= 0)
+        		return true;
+        	else
+        		decrementWait();
         return false;  
     }
     
@@ -39,15 +29,9 @@ public class Cultists extends Enemies
     {
         if(canAttack(c))
         {
-            castSpell(c); 
-        }
-    }
-    
-    public void draw(Graphics g) 
-    {
-        if(super.getHealth() > 0)
-        {
-            g.fillOval((int)super.getLocation().getX(), (int)super.getLocation().getY(), 30,30);
-        }
+        	setWait(5000);
+        	c.attacked(super.getAttack());
+			c.addBuff(b);
+        } 
     }
 }
