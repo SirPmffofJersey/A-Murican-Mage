1cdc3182f39395920f5631be3d99a9769a7ee99a
aquaTurtle1358 <ddfplay@gmail.com>
2016-06-19 01:53:20 -0400

    Update Bosses.java

--- Bosses.java
+++ Bosses.java
@@ -5,14 +5,16 @@
  * @author (your name here)
  * @version (version number or date here)
  */
-import java.awt.Point;
 import java.awt.Rectangle;
+import java.util.ArrayList;
+
+import Item.Sword;
 public abstract class Bosses extends Enemies 
 {
-    public Bosses(String name, int hp, int mp, int maxHp, int maxMp,int atk, int def, int spd, int lvl, Point loc, Rectangle dim, Item e,int direct)
+    public Bosses(String name, int hp, int mp, int maxHp, int maxMp,int atk, int def, int spd, int lvl, Rectangle dim, Sword e,int direct)
     {
-        super(name,hp,mp,maxHp,maxMp,atk,def,spd,lvl,loc,dim,e,direct);
+        super(name,hp,mp,maxHp,maxMp,atk,def,spd,lvl,dim,e,direct);
     }
     
-    public abstract void spawnEnemies(); 
+    public abstract void spawnEnemies(ArrayList<Enemies> e, Character c); 
 }
