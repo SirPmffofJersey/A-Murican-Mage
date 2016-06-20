1f0c7cf505ad7c486e0ea7aae95f29faaf26a88d
Wiro17 <wrsoccer01@gmail.com>
2016-06-19 21:45:20 -0400

    Update Bosses.java

--- Bosses.java
+++ Bosses.java
@@ -1,9 +1,9 @@
 
 /**
- * Abstract class Bosses - write a description of the class here
+ * Abstract class Bosses - Skeleton class for Dump and Crunch
  * 
- * @author (your name here)
- * @version (version number or date here)
+ * @author Will Roberts
+ * @version 2  6/17/16
  */
 import java.awt.Rectangle;
 import java.util.ArrayList;
@@ -11,10 +11,12 @@ import java.util.ArrayList;
 import Item.Sword;
 public abstract class Bosses extends Enemies 
 {
+    // Calls super constructor from Enemies
     public Bosses(String name, int hp, int mp, int maxHp, int maxMp,int atk, int def, int spd, int lvl, Rectangle dim, Sword e,int direct)
     {
         super(name,hp,mp,maxHp,maxMp,atk,def,spd,lvl,dim,e,direct);
     }
     
+    //Abstract method for each boss to spawn there own enemies
     public abstract void spawnEnemies(ArrayList<Enemies> e, Character c); 
 }
