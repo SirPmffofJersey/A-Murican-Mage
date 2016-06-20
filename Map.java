b51fb26806233655d12c1a5319de278e8b35a2fd
SirPmffofJersey <ndn1x1@gmail.com>
2016-06-19 20:55:39 -0400

    Update Map.java

--- Map.java
+++ Map.java
@@ -7,29 +7,29 @@
  */
 public class Map {
    //fields
-   private Room[] map;
-   private int playerRoomLoc;
+   private Room[] map; //array of rooms in the game
+   private int playerRoomLoc; //current number room the player is in
    
    //constructor
    public Map()
    {
     map = new Room[10];
     for(int i = 0; i < map.length; i++)
-    	map[i] = new Room(i);
+    	map[i] = new Room(i); //creates all the proper rooms
    }
    
    //methods
-   public Room getRoom(int roomIndex)
+   public Room getRoom(int roomIndex) //returns the room that is asked for
    {return map[roomIndex];}
    
-   public int getPlayerRoomLoc()
+   public int getPlayerRoomLoc() //returns which number room the player is in
    {return playerRoomLoc;}
    
    public Room getCurrentRoom() {
 	   return map[playerRoomLoc];
    }
 
-   public void setCurrentRoom(int roomSendingToIndex) {
+   public void setCurrentRoom(int roomSendingToIndex) { //changes which room number the player is in
 		playerRoomLoc = roomSendingToIndex;
    }
 }
