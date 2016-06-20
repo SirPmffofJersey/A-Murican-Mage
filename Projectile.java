aa3a934b8a6871c37b37a4d1e9108d2d1f348aa4
aquaTurtle1358 <ddfplay@gmail.com>
2016-06-19 21:05:45 -0400

    Update Projectile.java

--- Projectile.java
+++ Projectile.java
@@ -1,135 +1,114 @@
 package Item;
 
 import java.awt.Image;
-import java.awt.Point;
 import java.awt.Rectangle;
 
 import javax.swing.ImageIcon;
 
-import Main.Game;
-import Spell.AttackSpell;
-import Spell.Buff;
-
 /**
- * <h1> Projectile Class </h1>
- * This class represents a projectile sent by
- * an AttackSpell
+ * <h1> Sword Class: </h1>
+ * This represents a sword held by a character
  * 
  * @author Douglas Fisher
- * @author 6/11/16
+ * @since 6/11/16
  */
-public class Projectile {
+public class Sword {
 	private Rectangle dim;
-	private AttackSpell type;
-	private int xRange;
-	private int yRange;
-	private int xStart;
-	private int yStart;
-	private double distanceToTravel;
-	private boolean toBeRemoved;
 	private Image image;
-	private Buff b;
+	private String name;
+	private int damage;
+	private int range;
+	private int swingSpeed;
+	private final int WIDTH;
 	
 	/**
-	 * This is a constructor that builds a projectile and 
-	 * Initializes most of the fields.
-	 * @param t This represents the AttackSpell that "sent" the projectile
-	 * @param mouse This is where the player clicked to trigger a projectile being sent
+	 * This is a constructor that builds a sword
+	 * of a specified type
+	 * @param n This is the type of sword being constructed
 	 */
-	public Projectile(AttackSpell t, Point mouse) {
-		type = t;
-		mouse = Game.getScreen().getGameGraphics().mouseToPointConv(mouse);
-		double ratio = Math.abs(mouse.getY())/Math.abs(mouse.getX());
-		xRange = (int) Math.sqrt(Math.pow(t.getRange(), 2)/ (1 + ratio * ratio));
-		yRange = (int)(xRange * ratio);
-		if(mouse.getX() < 0)
-			xRange *= -1;
-		if(mouse.getY() < 0)
-			yRange *= -1;
-		double speed = 0;
-		if(t.getName().equals("Fire Lobber")) {
-			speed = 10;
-			b = new Buff("burn");
+	public Sword(String n) {
+		name = n;
+		if(name.equals("Dagger")) {
+			damage = 15;
+			range = 20;
+			swingSpeed = 10;
+			WIDTH = 10;
+			setImage(1);
+		} else if (name.equals("Saber")) {
+			damage = 25;
+			range = 50;
+			swingSpeed = 20;
+			WIDTH = 20;
+			setImage(1);
+		} else if (name.equals("Katana")){
+			damage = 20;
+			range = 100;
+			swingSpeed = 15;
+			WIDTH = 20;
+			setImage(1);
 		} else {
-			speed = 20;
-			b = null;
+			damage = 5;
+			range = 30;
+			swingSpeed = 20;
+			WIDTH = 10;
 		}
-		distanceToTravel = speed/t.getRange();
-		dim = new Rectangle((int)Game.getPlayer().getDimensions().getCenterX() - 25, (int)Game.getPlayer().getDimensions().getCenterY() - 25, 50, 50);
-		xStart = (int)dim.getX();
-		yStart = (int)dim.getY();
-		if(Math.abs(xRange) < Math.abs(yRange) && (yRange <  0 || t.getName().equals("Lightning Strike")))
-			image = new ImageIcon(getClass().getResource("/" + t.getName() + "1.gif")).getImage();
-		else if(Math.abs(xRange) > Math.abs(yRange) && (xRange >  0 || t.getName().equals("Lightning Strike")))
-			image = new ImageIcon(getClass().getResource("/" + t.getName() + "2.gif")).getImage();
-		else if(Math.abs(xRange) < Math.abs(yRange) && yRange >  0)
-			image = new ImageIcon(getClass().getResource("/" + t.getName() + "3.gif")).getImage();
-		else if(Math.abs(xRange) > Math.abs(yRange) && xRange <  0)
-			image = new ImageIcon(getClass().getResource("/" + t.getName() + "4.gif")).getImage();
 	}
 	
 	/**
-	 * This method returns this projectile's image.
-	 * @return Image This is the projectiles image
+	 * This method returns the sword's damage
+	 * @return int This is the sword's damage
 	 */
-	public Image getImage() {return image;}
+	public int getDamage() {return damage;}
 	
 	/**
-	 * This method returns this projectile's dimensions.
-	 * @return Rectangle This is the projectile's dimensions
+	 * This method returns the sword's dimensions
+	 * @return Rectangle This is the sword's dimensions
 	 */
 	public Rectangle getDim() {return dim;}
 	
 	/**
-	 * This method returns the AttackSpell that "sent" this 
-	 * projectile.
-	 * @return AttackSpell This is the AttackSpell that "sent" this projectile
+	 * This method returns this sword's image
+	 * @return Image This is the swords image
 	 */
-	public AttackSpell getSpell() {return type;}
+	public Image getImage() {return image;}
 	
 	/**
-	 * This method returns the buff associated with this projectile.
-	 * Returns null is no Buff is associated with the projectile.
-	 * @return Buff This is the buff associated with this projectile
+	 * This method returns this sword's name
+	 * @return String This is the sword's name
 	 */
-	public Buff getBuff() {return b;}
+	public String getName() {return name;}
 	
 	/**
-	 * This method detects if the projectile has reached the bounds of the
-	 * room and if so sets them to be deleted.
+	 * This method returns this sword's range
+	 * @return int This is the sword's range
 	 */
-	public void hitWall() {
-		int maxX = (int)(Game.getMap().getCurrentRoom().getDim().getX() + Game.getMap().getCurrentRoom().getDim().getWidth() - dim.getWidth());
-		int maxY = (int)(Game.getMap().getCurrentRoom().getDim().getY() + Game.getMap().getCurrentRoom().getDim().getHeight() - dim.getHeight());
-		if (dim.getX() >= maxX)
-			toBeRemoved = true;
-		if (dim.getX() <= 0)
-			toBeRemoved = true;
-		if (dim.getY() >= maxY)
-			toBeRemoved = true;
-		if (dim.getY() <= 0)
-			toBeRemoved = true;
+	public int getRange() {return range;}
+	
+	/**
+	 * This method returns how long this sword's "swing" is
+	 * @return int This is the sword's swingSpeed
+	 */
+	public int getSwingSpeed() {return swingSpeed;}
+	
+	/**
+	 * This method returns this sword's width
+	 * @return int This is the sword's width
+	 */
+	public int getWidth() {return WIDTH;}
+	
+	/**
+	 * This method sets the sword's dimensions
+	 * @param d This is what the sword's dimensions will be changed to
+	 * @param di This is the direction the character swinging the sword is facing
+	 */
+	public void setDim(Rectangle d, int di) {
+		dim = d;
+		setImage(di);
 	}
 	
 	/**
-	 * This method moves the projectile a preset distance in the x and y direction.
+	 * This method sets the sword's image
+	 * @param d The direction the sword is facing
 	 */
-	public void move() {
-		dim.setLocation((int)(dim.getX() + xRange * distanceToTravel), (int)(dim.getY() + yRange * distanceToTravel));
-		if(Math.abs(dim.getX() - xStart) > Math.abs(xRange) && Math.abs(dim.getY()) - yStart > Math.abs(yRange))
-			toBeRemoved = true;
-		hitWall();
-	}
-	
-	/**
-	 * This method sets the toBeRomved variable to true.
-	 */
-	public void setToBeRemoved() {toBeRemoved = true;}
-	
-	/**
-	 * This method returns a boolean representing if the projectile
-	 * needs to be removed.
-	 * @return boolean This is the field toBeRemoved
-	 */
-	public boolean toBeRemoved() {return toBeRemoved;}
+	public void setImage(int d) {image =  new ImageIcon(getClass().getResource("/" + name + d + ".gif")).getImage();}
 }
