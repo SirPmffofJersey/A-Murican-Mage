aac35c5043328bddc36e03b6cb4f84187684b3d0
Wiro17 <wrsoccer01@gmail.com>
2016-06-19 21:41:21 -0400

    Update Enemies.java

--- Enemies.java
+++ Enemies.java
@@ -17,11 +17,13 @@ public class Enemies extends Character
 {
     private boolean attacked = false; 
 
+    // calls super constructor
     public Enemies(String name, int hp, int mp, int maxHp, int maxMp,int atk, int def, int spd, int lvl, Rectangle dim, Sword s,int direct)
     {
         super(name,hp,mp,maxHp,maxMp,atk,def,spd,lvl,dim,s,direct);
     }
     
+    //if enemy hits a wall it doesn't move
     public void dontMove()
     {
         super.setLocation(new Point((int)super.getDimensions().getX() ,(int)super.getDimensions().getY())); 
@@ -32,20 +34,9 @@ public class Enemies extends Character
     {
         if(super.getHealth() > 0)
             g.fillOval((int)super.getDimensions().getX(),(int)super.getDimensions().getY(),50,50); 
-            
-        /*if(attacked)
-        {
-            if(facing == 3)
-                g.fillOval(x - 100,y,50,50); 
-            else if(facing == 2) 
-                g.fillOval(x + 100, y, 50,50);
-            else if(facing == 4)
-                g.fillOval(x,y + 100, 50,50); 
-            else 
-                g.fillOval(x,y - 100, 50, 50); 
-        } */
     }
     
+    // Drops a random loot based ona random number from 1-100
     public String dropLoot()
     {
     	int r = (int)(Math.random() * 100);
