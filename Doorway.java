6751136beb3a2bda9bb1600d8e209c747e73eb81
SirPmffofJersey <ndn1x1@gmail.com>
2016-06-19 20:52:45 -0400

    Update Doorway.java

--- Doorway.java
+++ Doorway.java
@@ -11,8 +11,8 @@ import Main.Game;
 public class Doorway extends Obstacle
 {
     //fields    
-    private int roomSendingToIndex;
-    private boolean enabled;
+    private int roomSendingToIndex; //the room that this door is sending the player to
+    private boolean enabled; //whether or not the player can be sent anywhere yet by the door
     
     //constructor
     public Doorway(int sendingToIndex)
@@ -23,13 +23,13 @@ public class Doorway extends Obstacle
     }
     
     //methods    
-    public int getRoomSendingToIndex()
+    public int getRoomSendingToIndex() //returns which room this doorway sends the player to
     {return roomSendingToIndex;}
     
-    public boolean getEnabled() 
+    public boolean getEnabled() //returns whether or not the player can travel using this door
     {return enabled;}
     
-    public void setEnabled(boolean e)
+    public void setEnabled(boolean e) //changes the enabled state of the doorway
     {
     	if(e)
     		setImage("Doorway enabled");
@@ -38,7 +38,7 @@ public class Doorway extends Obstacle
     	enabled = e;
     }
 
-    public void travel()
+    public void travel() //sends the player to the room that doorway sends to
     {
      Game.getMap().getCurrentRoom().disableDoors();
      Game.getMap().setCurrentRoom(roomSendingToIndex);
